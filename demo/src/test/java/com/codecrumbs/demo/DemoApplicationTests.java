6b084e501f6f7ac43b22f610b393f193ad1a02e1
AndradeReryson <reryson.andrade@fatec.sp.gov.br>
2024-05-25 11:23:25 -0300

    Novo Teste de Criação de Quiz

--- demo/src/test/java/com/codecrumbs/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/codecrumbs/demo/DemoApplicationTests.java
@@ -1,13 +1,30 @@
 package com.codecrumbs.demo;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.codecrumbs.demo.controller.QuizController;
+import com.codecrumbs.demo.dto.QuizCreateDTO;
+import com.codecrumbs.enumeracao.LinguagemEnum;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private QuizController quizController;
+
 	@Test
 	void contextLoads() {
+		QuizCreateDTO dto = new QuizCreateDTO();
+		dto.setTitulo("QUIZ TUTORIAL");
+		dto.setLinguagem(LinguagemEnum.CSS);
+		dto.setLista_perguntas(List.of());
+		dto.setId_criador(1);
+		//
+		quizController.criarNovoQuiz(dto);
 	}
 
 }
